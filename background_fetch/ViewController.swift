dfaf6f2b970d266bc321ce2b8f0afbc69f749b95
Ken Dong <kdong@fortinet.com>
2022-08-23 11:50:17 -0700

    Initial Commit

---
+++ background_fetch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  background_fetch
+//
+//  Created by Ken Dong on 2022-08-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
